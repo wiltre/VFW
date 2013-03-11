d2b4662224bf72468ca4d046d9231bdd9c4d5109
Wil Santiago <wiltres@gmail.com>
2013-03-10 20:31:34 -0400

    Create JS files and test them
    Alse test the connectivity between the css files

---
+++ Project 2/santiago_wiltre_project2/js/additemjs.js
@@ -0,0 +1,9 @@
+/**
+ * @author Wil
+ * itle: additem.html Style
+ * By: Wiltre Santiago 
+ * For: VFW Project 2
+ * This is the js for additem.html
+ */
+
+alert("Hello World");
---
+++ Project 2/santiago_wiltre_project2/js/indexjs.js
@@ -0,0 +1,9 @@
+/**
+ * @author Wil
+ * itle: additem.html Style
+ * By: Wiltre Santiago 
+ * For: VFW Project 2
+ * This is the js for index.html
+ */
+
+alert("Hello World");
