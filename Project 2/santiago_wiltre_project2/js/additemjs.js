28d68705733e6acf4b171dee4e01061ffca18cc6
Wil Santiago <wiltres@gmail.com>
2013-03-12 22:22:53 -0400

    Working on the JS
    Modify the additem.html to create the select group dinamicaly

--- Project 2/santiago_wiltre_project2/js/additemjs.js
+++ Project 2/santiago_wiltre_project2/js/additemjs.js
@@ -6,4 +6,27 @@
  * This is the js for additem.html
  */
 
-// alert("Hello World Add Item");
+// alert("Hello World Add Item"); //testing the js file
+
+
+//Wait until the DOM is ready. 
+window.addEventListener("DOMContentLoaded", function(){
+	
+	//getElementById Function
+	function element(x){
+		var getElement = document.getElementById(x);
+		return getElement;		
+	}
+	
+	//Global Variables
+	var itemCategory =["---Select a Category---","Clothing","Electronics","Transport","Jewerly","Other"];
+	
+	//Link Actions
+	var display = element("display")
+	display.addEventListener("click",displayData);
+	var reset = element("clear")
+	reset.addEventListener("click", resetData);
+	var submit = element("submit")
+	submit.addEventListener("click", saveData)
+	
+});
