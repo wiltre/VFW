000018d579757725b361bd8cf06ba12175975cd9
Wil Santiago <wiltres@gmail.com>
2013-03-14 00:25:44 -0400

    Finishing inputs from form

--- Project 2/santiago_wiltre_project2/js/additemjs.js
+++ Project 2/santiago_wiltre_project2/js/additemjs.js
@@ -21,7 +21,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	//Create the dropdown menu for the items category
 	function createCategory(){
 		var form = document.getElementsByTagName("form"); //this will be created as an array
-		var li = element("itemCategory");
+		var li = element("category");
 		var createSelect = document.createElement("select");
 		createSelect.setAttribute("id","itemCategory");
 		for(var i=0, j=itemCategories.length; i<j; i++){
@@ -33,17 +33,61 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		li.appendChild(createSelect);		
 	}
+	//Function to obtain the data from check and radio button
+	function radioValue(){
+		var rbutton = document.forms[0].sLocation;
+		for(var i=0; i<rbutton.length; i++){
+			if(rbutton[i].checked){
+				storeLocation = rbutton[i].value;	
+			}
+			
+		}
+	}
+	
+	function checkBoxValue(){
+		if(element("nearPurchase").checked){
+			nPurchase = "Yes";
+			console.log(nPurchase);
+		} else {
+			nPurchase = "No";
+			console.log(nPurchase);
+		}
+	}
+	
+	function saveData(){
+		var id          = Math.floor(Math.random()*100001);
+		//Get all the values of the form  and store them 
+		//Object properties contain array with the form label and value. 
+		radioValue();
+		checkBoxValue();
+		var info			= {};
+			info.category	=["Item Category: ", element("itemCategory").value];
+			info.sname		=["Store Name: ", element("storeName").value];
+			info.sweb		=["Store Website: ", element("webSite").value];
+			info.slocation	=["Store Location: ", storeLocation];
+			info.npurchase	=["Near Purchase: ", nPurchase];
+			info.priority	=["Item Priority: ", element("priority").value];
+			info.date		=["Date: ", element("date").value];
+			info.notes		=["Product Notes: ", element("itemsNotes").value];
+		//Save data in local storage
+		localStorage.setItem(id, JSON.stringify(info));
+		alert("INfo has been Saved!! :) ");
+			 				
+	};	
 		
 	//Global Variables
-	var itemCategories =["---Select a Category---","Clothing","Electronics","Transport","Jewerly","Other"];
+	var itemCategories =["---Select a Category---","Clothing","Electronics","Transport","Jewerly","Other","love nathie"];
+	var storeLocation
+	var nPurchase
 	createCategory();
 	
 	
+	
 	//Link Actions
-	var display = element("display")
+	/*var display = element("display")
 	display.addEventListener("click",displayData);
 	var reset = element("clear")
-	reset.addEventListener("click", resetData);
+	reset.addEventListener("click", resetData);*/
 	var submit = element("submit")
 	submit.addEventListener("click", saveData)
 	
