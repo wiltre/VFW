11939446893135f4a66d90b8cc02a6d2f20a4566
Wil Santiago <wiltres@gmail.com>
2013-03-17 19:41:27 -0400

    Creating the file structure for project 3
    Fix the few mistakes for the project 2 and now ready to keep going to
    project 3

---
+++ Project 3/santiago_wiltre_project3/js/indexjs.js
@@ -0,0 +1,25 @@
+/**
+ * @author Wil
+ * itle: additem.html Style
+ * By: Wiltre Santiago 
+ * For: VFW Project 2
+ * This is the js for index.html
+ */
+
+// alert("Hello World Index. ");
+
+window.addEventListener("DOMContentLoaded", function(){	
+	//getElementById Function
+	function element(a){
+		var getElement = document.getElementById(a);
+		return getElement;		
+	}
+	
+	//button function 
+	function launchApp(){
+			window.location="additem.html";
+		}
+	// calling buttn function	
+	var startApp = element("startApp");
+		startApp.addEventListener("click", launchApp);
+});
